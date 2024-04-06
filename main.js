ca7b6f771153b0e5f5acfae6283cd6c16e5e23ce
JadeJadeMain <jadecotonganformentera@gmail.com>
2024-04-06 19:51:56 -0200

    new website update

---
+++ main.js
@@ -0,0 +1,11 @@
+let navigate = document.getElementById('navigation');
+let links = document.getElementsByTagName('a');
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY >= 100){
+        navigate.classList.add('newback');
+
+    }else{
+        navigate.classList.remove('newback');
+    }
+})
