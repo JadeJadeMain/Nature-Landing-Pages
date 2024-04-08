74d1eb9bd8fd2d77f64fd507b73752ce9493634e
JadeJadeMain <146413477+JadeJadeMain@users.noreply.github.com>
2024-04-08 10:17:25 -0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 let navigate = document.getElementById('navigation');
 let links = document.getElementsByTagName('a');
 
@@ -9,14 +9,4 @@ window.addEventListener('scroll', ()=>{
     }else{
         navigate.classList.remove('newback');
     }
-=======
-let navigate = document.getElementById('navigation');
-
-window.addEventListener('scroll', ()=>{
-    if(window.scrollY >= 100){
-        navigate.classList.add('newback');
-    }else{
-        navigate.classList.remove('newback')
-    }
->>>>>>> 21c491cb1dde9d25fe137a55c1872e413a0a17c3
 })
